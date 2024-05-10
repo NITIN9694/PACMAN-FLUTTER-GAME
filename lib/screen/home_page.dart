61130d8ff531e28ef6b2137d304a3e8da775b31f
Nitin Jha <61863033+NITIN9694@users.noreply.github.com>
2024-05-10 16:52:43 +0530

    setup

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -9,11 +9,52 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  static int numberIbRow = 11;
+  int numberInColum = numberIbRow*16;
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      backgroundColor: whiteColor,
-      body: Column(),
+      backgroundColor: Colors.grey[800],
+      body: Column(
+        children: [
+          Expanded(
+              flex: 5,
+              child: Container(
+                child: GridView.builder(
+                  physics:const NeverScrollableScrollPhysics(),
+                  itemCount: numberInColum,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount:numberIbRow
+                    ),
+                    itemBuilder: (BuildContext context,int index){
+                    return Container(
+                      margin: EdgeInsets.all(1.0),
+                      color: Colors.grey,
+                    );
+                    }),
+              )),
+          Expanded(child: Container(
+
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text("Score:",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+                ),
+                Text("P L A Y",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                  ),
+                )
+              ],
+            ),
+          ))
+        ],
+      ),
     );
   }
 }
